af4872fc4c29851f8db53f46f6fd647574ed1441
abdul-halim-dev <jamesborn1518@gmail.com>
2024-11-02 22:39:14 +0600

    second time modified

--- src/cardcomponent/FooterRight.jsx
+++ src/cardcomponent/FooterRight.jsx
@@ -3,12 +3,12 @@ import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from "reac
 import { Link } from "react-router-dom"
 const FooterRight = () => {
   return (
-    <div className="w-[50%] flex items-center justify-center flex-col gap-2 ">
-      <div className="w-full items-end justify-end gap-2 flex">
+    <div className=" w-full sm:w-[50%] flex items-center justify-center flex-col gap-2 ">
+      <div className="w-full  items-end justify-end gap-2 flex">
       <p> মোবাইল এপ ডাউনলোড করুন </p>
       </div>
        
-          <div className="w-full items-end justify-end gap-2 flex">
+          <div className="w-full  items-end justify-end gap-2 flex">
           <Link className=" w-[150px] h-full ">  <img className="w-full h-full object-contain" src="https://i.ibb.co.com/7zhGpr1/googelget.jpg " alt="" /></Link>
            <Link className=" w-[150px] h-full ">  <img className="w-full h-full object-contain" src="https://i.ibb.co.com/jWDqNK1/apple.jpg " alt="" /></Link>
         
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import Footerleft from "../cardcomponent/Footerleft"
 import FooterRight from "../cardcomponent/FooterRight"
 
@@ -5,8 +6,8 @@ import FooterRight from "../cardcomponent/FooterRight"
 
 const Footer = () => {
   return (
-    <footer className=" p-2  w-full flex items-center justify-center flex-col gap-2 bg-slate-300">
-         <div className="w-full flex items-center justify-between py-2">
+    <footer className="   w-full flex items-center justify-center flex-col  ">
+         <div className="w-full flex flex-col sm:flex-row items-center justify-between gap-2 p-2 bg-slate-400 ">
             <div className="flex flex-col items-start justify-start">
                 <p className="text-[12px] text-black"> আমরা জনগণের পক্ষে</p>
                 <h2 className="text-green-500 text-2xl font-bold"> <span className="text-red-500"> বাংলাদেশ  </span> প্রতিদিন </h2>
@@ -14,9 +15,17 @@ const Footer = () => {
             <p className="text-lg text-black">ভারপ্রাপ্ত সম্পাদক : আবু তাহের </p>
         </div>
         
-         <div className=" w-full flex items-center justify-between">
+         <div className="p-2 w-full flex-col sm:flex-row gap-2 flex items-center justify-between bg-slate-300 " >
          <Footerleft/>  
          <FooterRight/>  
+         </div>
+         <div className="w-full flex-col sm:flex-row gap-2 flex items-center justify-between p-2 bg-slate-200">
+            <ul className="flex items-center justify-center gap-4">
+                <li type="disc" className="px-2 text-black text-sm font-medium hover:underline"> <Link> যোগাযোগ  </Link></li>
+                <li type="disc"  className="px-2 text-black text-sm font-medium hover:underline"> <Link>  গোপনীয় নীতি   </Link></li>
+                <li type="disc"  className="px-2 text-black text-sm font-medium hover:underline"> <Link>  বিজ্ঞাপনের মূল্য তালিকা  </Link></li>
+            </ul>
+            <p className="text-sm text-black">স্বত্ব © ২০২৪ বাংলাদেশ প্রতিদিন </p>
          </div>
     </footer>
   )
